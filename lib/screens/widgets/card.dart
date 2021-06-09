8241d2202e32ac08fa8bc2cfa63e4966b83eb715
Fayazazeemp <fayazazeem444555@gmail.com>
2021-06-09 17:47:44 +0530

    padding

--- lib/screens/widgets/card.dart
+++ lib/screens/widgets/card.dart
@@ -15,7 +15,7 @@ class DetailsCard extends StatelessWidget {
           color: Color(0xFF122F5C),
         borderRadius: BorderRadius.all(Radius.circular(20))
       ),
-      margin: EdgeInsets.only(right: 30, top: 20, bottom: 20,left:5),
+      margin: EdgeInsets.all(20),
       width: 160.0,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
